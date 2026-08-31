52b77287036e97864f86c5ee6347344ca05e90ba
imanmoradzahi1345-oss <imanmoradzahi1345@gmail.com>
2026-08-31 15:46:48 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from telegram.ext import (
 )
 from telegram.constants import ChatMemberStatus, ChatType
 
-BOT_TOKEN = "8273833935:AAFMMPeUg1wReoebNTz9L5GoQmYMDLR-_t0"
+BOT_TOKEN = "8975007734:AAFdlIGcFkwMKBt84IaoAMTM2E2Y_ed8_cE"
 MAIN_ADMIN = 7530457395
 DATA = "card_data.json"
 DAILY = 100
